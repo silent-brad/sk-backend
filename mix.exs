97421d8d9c9ecb0f3845b7b5424a90ef8611847d
bwhtdev <bradscottwhite@gmail.com>
2025-09-09 21:24:50 -0400

    refactored

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sk.MixProject do
     [
       app: :sk,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
